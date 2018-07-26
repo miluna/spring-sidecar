9302b55de864f442727f4ecf7a04dc8edc1f56ba
Miguel Angel Luna <miluna1994@gmail.com>
2018-07-26 20:19:44 +0200

    initial setup

---
+++ spring-node-sidecar/src/main/java/com/mla/springnodesidecar/SpringNodeSidecarApplication.java
@@ -0,0 +1,12 @@
+package com.mla.springnodesidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNodeSidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNodeSidecarApplication.class, args);
+	}
+}
